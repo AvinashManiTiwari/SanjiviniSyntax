3a661bfca43ff471c82926063df0d2dbeccfde7e
Avinash Mani Tripathi <tiwariavinashmani@gmail.com>
2026-07-26 00:17:31 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,100 @@
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim().toLowerCase();
+
+    const password = document.getElementById("password").value;
+
+    const rememberMe = document.getElementById("rememberMe").checked;
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const user = users.find(function (item) {
+
+        return item.email === email && item.password === password;
+
+    });
+
+    if (!user) {
+
+        alert("Please Register first or enter a valid Email and Password!");
+
+        return;
+
+    }
+
+    // Current Login User
+    localStorage.setItem("currentUser", JSON.stringify(user));
+
+    // Remember Me
+    if (rememberMe) {
+
+        localStorage.setItem("rememberEmail", email);
+
+    } else {
+
+        localStorage.removeItem("rememberEmail");
+
+    }
+
+    alert("Login Successful!");
+
+    loginForm.reset();
+
+    window.location.href = "index.html";
+
+});
+
+ /* Remember Email */
+
+window.addEventListener("DOMContentLoaded", function () {
+
+    const rememberedEmail = localStorage.getItem("rememberEmail");
+
+    if (rememberedEmail) {
+
+        document.getElementById("email").value = rememberedEmail;
+
+        document.getElementById("rememberMe").checked = true;
+
+    }
+
+});
+
+
+
+ /* Show / Hide Password */
+
+const passwordInput = document.getElementById("password");
+
+const togglePassword = document.querySelector(".toggle-password");
+
+const eyeIcon = togglePassword.querySelector("i");
+
+togglePassword.addEventListener("click", function(){
+
+    if(passwordInput.type === "password"){
+
+        passwordInput.type = "text";
+
+        eyeIcon.classList.remove("fa-eye");
+
+        eyeIcon.classList.add("fa-eye-slash");
+
+    }
+
+    else{
+
+        passwordInput.type = "password";
+
+        eyeIcon.classList.remove("fa-eye-slash");
+
+        eyeIcon.classList.add("fa-eye");
+
+    }
+
+});
---
+++ js/register.js
@@ -0,0 +1,119 @@
+
+
+const registerForm = document.getElementById("registerForm");
+
+registerForm.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const fullname = document.getElementById("fullname").value.trim();
+
+    const email = document.getElementById("email").value.trim().toLowerCase();
+
+    const phone = document.getElementById("phone").value.trim();
+
+    const password = document.getElementById("password").value;
+
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (
+        fullname === "" ||
+        email === "" ||
+        password === "" ||
+        confirmPassword === ""
+    ) {
+
+        alert("Please fill all required fields.");
+
+        return;
+
+    }
+
+    if (password !== confirmPassword) {
+
+        alert("Passwords do not match.");
+
+        return;
+
+    }
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const userExists = users.find(user => user.email === email);
+
+    if (userExists) {
+
+        alert("Email already registered.");
+
+        return;
+
+    }
+
+    const newUser = {
+
+        fullname: fullname,
+
+        email: email,
+
+        phone: phone,
+
+        password: password
+
+    };
+
+    users.push(newUser);
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert("Registration Successful!");
+
+    registerForm.reset();
+
+    window.location.href = "login.html";
+
+});
+
+
+
+
+
+
+// password eye
+
+ /* Show Or Hide Password */
+
+const passwordFields = document.querySelectorAll(".password-box");
+
+passwordFields.forEach(function(box){
+
+    const input = box.querySelector("input");
+
+    const toggle = box.querySelector(".toggle-password");
+
+    const icon = toggle.querySelector("i");
+
+    toggle.addEventListener("click", function(){
+
+        if(input.type === "password"){
+
+            input.type = "text";
+
+            icon.classList.remove("fa-eye");
+
+            icon.classList.add("fa-eye-slash");
+
+        }
+
+        else{
+
+            input.type = "password";
+
+            icon.classList.remove("fa-eye-slash");
+
+            icon.classList.add("fa-eye");
+
+        }
+
+    });
+
+});
